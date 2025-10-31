4993b2eed2dd541edd16445bc9f18c5488ec736c
imamityaadav <89591490+imamityaadav@users.noreply.github.com>
2025-10-31 10:23:12 +0530

    Delete singlestate-terraform directory

--- singlestate-terraform/modules/opensearch/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "opensearch_endpoint" {
-  description = "OpenSearch domain endpoint"
-  value       = aws_opensearch_domain.main.endpoint
-}
-
-output "opensearch_arn" {
-  description = "OpenSearch domain ARN"
-  value       = aws_opensearch_domain.main.arn
-}
